868bc41d056fc604579a45503576602b8b1b372e
GERAULT Tristan <tristan.gerault@inrae.fr>
2026-03-02 15:16:36 +0100

    functional tests

--- test/ganache_test.py
+++ test/ganache_test.py
@@ -10,24 +10,34 @@ from granap.network_base import AbstractNetwork
 from granap.root_class import RootAnatomy
 from mecha.utils.visu import visualize
 
-# Create a Mecha instance with a GRANAP network
-root = RootAnatomy()
-_ = root.export_to_adjencymatrix()
+import numpy as np
 
-# Create a default input for Mecha
-# default_input = InData()
-# root_network = NetworkBuilder(root)
+def test_full_mecha_run():
+    # Create a Mecha instance with a GRANAP network
+    root = RootAnatomy()
+    _ = root.export_to_adjencymatrix()
 
-Granar_input = InData(cellset_file="simulations/tutorials/tutorial_data/current_root.xml")
-Granar_input.geometry.set_maturity_stages([1])
+    # Create a default input for Mecha
+    # default_input = InData()
+    # root_network = NetworkBuilder(root)
 
-# Create a Mecha instance with the default input
+    Granar_input = InData(cellset_file="inputs/current_root.xml")
+    Granar_input.geometry.set_maturity_stages([1])
 
-mecha = Mecha(Granar_input)
+    # Create a Mecha instance with the default input
 
-# Test the connection
-# visualize(mecha.network, "network")
+    mecha = Mecha(Granar_input)
 
-mecha.compute_conductivities()
-for i in range(len(mecha.root_hydraulic_properties)):
-    print(mecha.root_hydraulic_properties[i])
+    # Test the connection
+    # visualize(mecha.network, "network")
+
+    mecha.compute_conductivities()
+    for i in range(len(mecha.root_hydraulic_properties)):
+        print(mecha.root_hydraulic_properties[i])
+
+    assert not np.isnan(mecha.root_hydraulic_properties[-1]["kr"])
+    assert not np.isnan(mecha.root_hydraulic_properties[-1]["Kx"])
+
+
+if __name__ == "__main__":
+    test_full_mecha_run()
