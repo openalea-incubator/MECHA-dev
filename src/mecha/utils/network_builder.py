bed40b2f5ed70f9c9f4a275cb4141f86457a032e
HeymansAdrien <36161856+HeymansAdrien@users.noreply.github.com>
2026-06-10 11:15:30 +0200

    Phi-thickening
    #15

--- src/mecha/utils/network_builder.py
+++ src/mecha/utils/network_builder.py
@@ -27,7 +27,7 @@ from typing import Dict, Any, List, Optional, Tuple
 from lxml import etree
 import geopandas as gpd
 
-from mecha.utils.data_loader import GeneralData, GeometryData
+from mecha.utils.data_loader import GeneralData, GeometryData, HydraulicData
 from mecha.utils.hydraulic_cell import HydraulicCellManager
 
 from granap.network_base import AbstractNetwork
@@ -115,6 +115,10 @@ class NetworkBuilder(AbstractNetwork):
         self.distance_from_center: Optional[np.ndarray] = None
         self.row_outer_cortex: Optional[np.ndarray] = None
 
+        # phi parameters
+        self.n_phi_layers: int = 0
+        self.phi_type: int = 1  # 1=inner, 2=outer, 3=middle range
+
         # Relative positions for walls
         self.r_rel: Optional[np.ndarray] = None
         self.x_rel: Optional[np.ndarray] = None
@@ -166,7 +170,7 @@ class NetworkBuilder(AbstractNetwork):
         # If we are populating from another network, this might not be needed.
         pass
 
-    def build_network(self, general: GeneralData, geometry: GeometryData, cellset_data, verbose: bool = False, centroid_method: str = "shapely"):
+    def build_network(self, general: GeneralData, geometry: GeometryData, cellset_data, hydraulic = HydraulicData, verbose: bool = False, centroid_method: str = "shapely"):
         """Main method to build network from XML data"""
         if cellset_data is None:
             raise ValueError("Cellset data is None")
@@ -174,10 +178,17 @@ class NetworkBuilder(AbstractNetwork):
             raise ValueError("General data is None")
         if geometry is None:
             raise ValueError("Geometry data is None")
+        if hydraulic is None:
+            raise ValueError("Hydraulic data is None")
         
         self.cellset = cellset_data
         self.n_walls = len(self.cellset['points'])
         self.n_cells = len(self.cellset['cells'])
+
+        if verbose:
+            print('Set pre hydraulic parameters in network...')
+        self.phi_type = hydraulic.phi_type
+        self.n_phi_layers = hydraulic.n_phi_layers
 
         if verbose:
             print('  Creating wall, junction and cell nodes...')
---
+++ test/test_phi_thick.py
@@ -0,0 +1,49 @@
+from mecha.mecha_class import Mecha
+from mecha.utils.data_loader import InData
+from mecha.utils.network_builder import NetworkBuilder
+from granap.root_class import RootAnatomy
+from mecha.utils.visu import visualize
+import matplotlib.pyplot as plt
+
+
+
+def test_phi_thick():
+    root = RootAnatomy()
+    root.update_params("aerenchyma", "aerenchyma_proportion", 0.0)    
+    
+    # Generated a root anatomy without aerenchyma
+    # prepare granap data structure for mecha
+    root.export_to_adjencymatrix()
+
+    # prepare default inputs for mecha
+    default_input = InData()
+    default_input.geometry.set_maturity_stages([1,8])
+
+    network = NetworkBuilder(root)
+    network.n_phi_layers = 2
+    network.phi_type = 3
+    network.populate_from_network()
+    
+    m = Mecha(default_input, network=network)
+    
+    # 3. Solve
+    print("--- Solving Hydraulic System ---")
+    print(f"Number of scenarios to solve: {m.boundary.n_scenarios}")
+
+    m.water_flux()
+
+    _, ax = plt.subplots(1, 2, figsize=(15, 6))
+    visualize(m, visu_type="flow", ax=ax[0], maturity_idx= 0, scenario_idx="standard water flow", show_plot=False)
+    visualize(m, visu_type="flow", ax=ax[1], maturity_idx= 1, scenario_idx="standard water flow", show_plot=False)
+    # add Main title for the figure
+    plt.suptitle("$\Phi$ Thickening Type III", color='black', bbox=dict(facecolor='white', alpha=0.5, edgecolor='none'))
+    # Add title for the figure
+    ax[0].set_title("Flow - Maturity $En_{cs}$ - Std Water Flow", color='black', bbox=dict(facecolor='white', alpha=0.5, edgecolor='none'))
+    ax[1].set_title("Flow - Maturity $En_{sub} & Ex_{sub}$ - Std Water Flow", color='black', bbox=dict(facecolor='white', alpha=0.5, edgecolor='none'))
+    plt.tight_layout()
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    test_phi_thick()
